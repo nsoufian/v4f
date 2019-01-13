cfb974899731a7c4112ddb4db6cde00346366826
reyx <soufiane.nass7@gmail.com>
2019-01-13 01:54:00 +0100

    Add max mix and equals rules

--- src/rules.js
+++ src/rules.js
@@ -1,29 +1,39 @@
 export const string = options => ({
-	validator: value => typeof value === "string",
-	options
+  validator: value => typeof value === "string",
+  options
 });
 
 export const number = options => ({
-	validator: value => typeof value === "number",
-	options
-});
-
-export const bigint = options => ({
-	validator: value => typeof value === "bigint",
-	options
+  validator: value => typeof value === "number",
+  options
 });
 
 export const boolean = options => ({
-	validator: value => typeof value === "boolean",
-	options
+  validator: value => typeof value === "boolean",
+  options
 });
 
 export const object = options => ({
-	validator: value => typeof value === "object",
-	options
+  validator: value => typeof value === "object",
+  options
 });
 
 export const required = options => ({
-	validator: value => value !== "" && value !== null && value !== undefined,
-	options
+  validator: value => value !== "" && value !== null && value !== undefined,
+  options
+});
+
+export const min = (minValue, options) => ({
+  validator: value => value.length >= minValue,
+  options
+});
+
+export const max = (maxValue, options) => ({
+  validator: value => value.length <= maxValue,
+  options
+});
+
+export const equals = (equalsValue, options) => ({
+  validator: value => value.length === equalsValue,
+  options
 });
