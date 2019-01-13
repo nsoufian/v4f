cb40b999174c0e614dde9dc622b00eef545d343e
reyx <soufiane.nass7@gmail.com>
2019-01-13 01:41:57 +0100

    Add new rules for primitives

--- src/rules.js
+++ src/rules.js
@@ -3,6 +3,26 @@ export const string = options => ({
 	options
 });
 
+export const number = options => ({
+	validator: value => typeof value === "number",
+	options
+});
+
+export const bigint = options => ({
+	validator: value => typeof value === "bigint",
+	options
+});
+
+export const boolean = options => ({
+	validator: value => typeof value === "boolean",
+	options
+});
+
+export const object = options => ({
+	validator: value => typeof value === "object",
+	options
+});
+
 export const required = options => ({
 	validator: value => value !== "" && value !== null && value !== undefined,
 	options
