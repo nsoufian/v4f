015a40ada282961c954d12c55c678a4881d71fa6
reyx <soufiane.nass7@gmail.com>
2019-01-13 23:14:52 +0100

    Improve configuration to publish to npm

--- rollup.config.js
+++ rollup.config.js
@@ -2,15 +2,15 @@ import resolve from "rollup-plugin-node-resolve";
 import babel from "rollup-plugin-babel";
 
 export default {
-	input: "src/index.js",
-	output: {
-		file: "build/bundle.js",
-		format: "iife"
-	},
-	plugins: [
-		resolve(),
-		babel({
-			exclude: "node_modules/**" // only transpile our source code
-		})
-	]
+  input: "src/index.js",
+  output: {
+    file: "lib/index.js",
+    format: "cjs"
+  },
+  plugins: [
+    resolve(),
+    babel({
+      exclude: "node_modules/**" // only transpile our source code
+    })
+  ]
 };
