55533f1e4cfc23e2e9aa732bcabf6ad676307e34
reyx <soufiane.nass7@gmail.com>
2019-01-16 18:05:18 +0100

    Refactore schema code

--- src/schema.js
+++ src/schema.js
@@ -1,15 +1,17 @@
 import { isEmpty } from "./utils";
 
-const booleanValidator = (schema, values) => {
+const booleanValidate = (schema, values) => {
   const rules = Object.entries(schema);
   for (let i = 0; i < rules.length; i += 1) {
     const [name, rule] = rules[i];
-    const result = rule.validate(values[name], { message: false });
-    if (result === false) return false;
+    if (!rule.validate(values[name])) {
+      return false;
+    }
   }
   return true;
 };
-const msgValidator = (schema, values) => {
+
+const msgValidate = (schema, values) => {
   const errors = {};
   Object.entries(schema).forEach(([name, rule]) => {
     const result = rule.validate(values[name], {
@@ -17,18 +19,24 @@ const msgValidator = (schema, values) => {
     });
     if (result !== true) errors[name] = result;
   });
-  return isEmpty(errors) === true ? null : errors;
+  return isEmpty(errors) ? null : errors;
 };
 
 export default schema => {
   function Validator() {}
 
-  Validator.prototype.validate = function(values, { message = false } = {}) {
-    if (message === true) return msgValidator(schema, values);
-    return booleanValidator(schema, values);
-  };
+  function validate(values, { message = false } = {}) {
+    if (message) {
+      return msgValidate(schema, values);
+    }
+    return booleanValidate(schema, values);
+  }
+
   Object.entries(schema).forEach(([name, rule]) => {
     Validator.prototype[name] = rule;
   });
+
+  Validator.prototype.validate = validate;
+
   return new Validator();
 };
