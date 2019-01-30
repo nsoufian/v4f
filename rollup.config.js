1b89a4f8d460077f5ada355fe5f18a75d4efa2e0
reyx <soufiane.nass7@gmail.com>
2019-01-30 01:02:47 +0100

    Increment version and rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -1,16 +1,35 @@
 import resolve from "rollup-plugin-node-resolve";
 import babel from "rollup-plugin-babel";
 
-export default {
-  input: "src/index.js",
-  output: {
-    file: "lib/index.js",
-    format: "cjs"
+export default [
+  // CommonJS
+  {
+    input: "src/index.js",
+    output: {
+      file: "lib/cjs.js",
+      format: "cjs",
+      indent: false
+    },
+    plugins: [
+      resolve(),
+      babel({
+        exclude: "node_modules/**" // only transpile our source code
+      })
+    ]
   },
-  plugins: [
-    resolve(),
-    babel({
-      exclude: "node_modules/**" // only transpile our source code
-    })
-  ]
-};
+  // ES
+  {
+    input: "src/index.js",
+    output: {
+      file: "lib/es.js",
+      format: "es",
+      indent: false
+    },
+    plugins: [
+      resolve(),
+      babel({
+        exclude: "node_modules/**" // only transpile our source code
+      })
+    ]
+  }
+];
