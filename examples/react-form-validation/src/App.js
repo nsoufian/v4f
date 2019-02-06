8f64e8f0470f10e3d1aa5527b1b4c8fc8b999a51
reyx <soufiane.nass7@gmail.com>
2019-02-06 23:41:36 +0100

    Finish React Example

--- examples/react-form-validation/src/App.js
+++ examples/react-form-validation/src/App.js
@@ -1,14 +1,67 @@
-import React, { Component } from 'react';
-import From from './form';
+import React, { Component } from "react";
+import From from "./form";
+import Validator from "./validator";
+
+const initState = {
+	firstName: "",
+	lastName: "",
+	username: "",
+	email: "",
+	password: "",
+	cPassword: "",
+	errors: {}
+};
 
 class App extends Component {
-  render() {
-    return (
-      <div>
-        <From errors={{}}/>
-      </div>
-    );
-  }
+	constructor(props) {
+		super(props);
+		this.state = { ...initState };
+		this.handleChange = this.handleChange.bind(this);
+		this.handleSubmit = this.handleSubmit.bind(this);
+		this.handleDirty = this.handleDirty.bind(this);
+	}
+	handleChange(e) {
+		this.setState({ [e.target.name]: e.target.value });
+	}
+	handleSubmit() {
+		Validator.validate(this.state)
+			.then(data => {
+				alert("Success");
+				this.setState(initState);
+			})
+			.catch(errors => {
+				this.setState({ errors });
+			});
+	}
+	handleDirty(e) {
+		const { name, value } = e.target;
+
+		const isValid = Validator[name].validate(value, {
+			verbose: true,
+			values: this.state
+		});
+		if (isValid !== true) {
+			this.setState({
+				errors: { ...this.state.errors, [name]: isValid }
+			});
+		} else {
+			this.setState({
+				errors: { ...this.state.errors, [name]: undefined }
+			});
+		}
+	}
+	render() {
+		return (
+			<div>
+				<From
+					{...this.state}
+					handleChange={this.handleChange}
+					handleSubmit={this.handleSubmit}
+					handleDirty={this.handleDirty}
+				/>
+			</div>
+		);
+	}
 }
 
 export default App;
--- examples/react-form-validation/src/form.jsx
+++ examples/react-form-validation/src/form.jsx
@@ -1,97 +1,118 @@
 import React from "react";
-import Card from '@material-ui/core/Card';
-import CardContent from '@material-ui/core/CardContent';
-import Grid from '@material-ui/core/Grid';
-import TextField from '@material-ui/core/TextField';
+import Card from "@material-ui/core/Card";
+import CardContent from "@material-ui/core/CardContent";
+import Grid from "@material-ui/core/Grid";
+import TextField from "@material-ui/core/TextField";
 import CardHeader from "@material-ui/core/CardHeader";
 import { CardActions } from "@material-ui/core";
-import Button from '@material-ui/core/Button';
+import Button from "@material-ui/core/Button";
 
 const From = ({
-    firstName,
-    lastName,
-    username,
-    email,
-    password,
-    cPassword,
-    errors,
-    handleChange,
-    handleSubmit
-})=> (
-     <Grid container justify="center">
-        <Grid item xs={12} sm={10} md={5}>
-        <Card>
-            <CardHeader title="Register" />
-            <CardContent>
-            <TextField
-                id="outlined-name"
-                label="FirstName"
-                value={firstName}
-                onChange={handleChange}
-                error={errors.firstName}
-                margin="normal"
-                fullWidth
-                variant="outlined"
-            />
-            <TextField
-                id="outlined-name"
-                label="LastName"
-                value={lastName}
-                onChange={handleChange}
-                error={errors.lastName}
-                margin="normal"
-                fullWidth
-                variant="outlined"
-            />
-            <TextField
-                id="outlined-name"
-                label="Username"
-                value={username}
-                onChange={handleChange}
-                error={errors.username}
-                margin="normal"
-                fullWidth
-                variant="outlined"
-            />
-            <TextField
-                id="outlined-name"
-                label="Email"
-                value={email}
-                onChange={handleChange}
-                error={errors.email}
-                margin="normal"
-                fullWidth
-                variant="outlined"
-            />
-            <TextField
-                id="outlined-name"
-                label="Password"
-                value={password}
-                onChange={handleChange}
-                error={errors.password}
-                margin="normal"
-                fullWidth
-                variant="outlined"
-            />
-            <TextField
-                id="outlined-name"
-                label="Password Confirmation"
-                value={cPassword}
-                onChange={handleChange}
-                error={errors.cPassword}
-                margin="normal"
-                fullWidth
-                variant="outlined"
-            />
-            </CardContent>
-            <CardActions>
-            <Button variant="contained" color="primary" fullWidth onClick={handleSubmit}>
-                Submit
-            </Button>
-            </CardActions>
-        </Card>
-        </Grid>
-    </Grid>
-)
+	firstName,
+	lastName,
+	username,
+	email,
+	password,
+	cPassword,
+	errors,
+	handleChange,
+	handleSubmit,
+	handleDirty
+}) => (
+	<Grid container justify="center">
+		<Grid item xs={12} sm={10} md={5}>
+			<Card>
+				<CardHeader title="React From Validation Using V4F library" />
+				<CardContent>
+					<TextField
+						label="FirstName"
+						name="firstName"
+						value={firstName}
+						onChange={handleChange}
+						onBlur={handleDirty}
+						error={errors.firstName !== undefined}
+						helperText={errors.firstName}
+						margin="normal"
+						fullWidth
+						variant="outlined"
+					/>
+					<TextField
+						label="LastName"
+						name="lastName"
+						value={lastName}
+						onChange={handleChange}
+						onBlur={handleDirty}
+						error={errors.lastName !== undefined}
+						helperText={errors.lastName}
+						margin="normal"
+						fullWidth
+						variant="outlined"
+					/>
+					<TextField
+						label="Username"
+						name="username"
+						value={username}
+						onChange={handleChange}
+						onBlur={handleDirty}
+						error={errors.username !== undefined}
+						helperText={errors.username}
+						margin="normal"
+						fullWidth
+						variant="outlined"
+					/>
+					<TextField
+						label="Email"
+						name="email"
+						value={email}
+						onChange={handleChange}
+						onBlur={handleDirty}
+						error={errors.email !== undefined}
+						helperText={errors.email}
+						margin="normal"
+						type="email"
+						fullWidth
+						variant="outlined"
+					/>
+					<TextField
+						label="Password"
+						name="password"
+						value={password}
+						onChange={handleChange}
+						onBlur={handleDirty}
+						error={errors.password !== undefined}
+						helperText={errors.password}
+						margin="normal"
+						type="password"
+						fullWidth
+						variant="outlined"
+					/>
+					<TextField
+						label="Password Confirmation"
+						name="cPassword"
+						value={cPassword}
+						onChange={handleChange}
+						onBlur={handleDirty}
+						error={errors.cPassword !== undefined}
+						helperText={errors.cPassword}
+						margin="normal"
+						type="password"
+						fullWidth
+						variant="outlined"
+					/>
+				</CardContent>
+				<CardActions>
+					<Button
+						variant="contained"
+						color="primary"
+						fullWidth
+						onClick={handleSubmit}
+					>
+						Submit
+					</Button>
+				</CardActions>
+			</Card>
+		</Grid>
+	</Grid>
+);
 
 export default From;
